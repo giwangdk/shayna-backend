92d8123f2b9e7bd598184d16f270e7f5b2bf735a
gidwikintan <gidwikintan@gmail.com>
2021-07-18 04:29:22 +0700

    checkout API

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('products', 'API\ProductController@all');
+Route::post('checkout', 'API\CheckoutController@checkout');
